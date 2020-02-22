4ecc6ed0108a9fc9c18518558b8e3e1b6753f811
mpottebaum <mpottebaum@gmail.com>
2020-02-22 17:32:23 -0600

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,27 @@
 class Employee
+    attr_accessor :manager
+    attr_reader :name, :salary
+    @@all = []
 
+    def initialize(name, salary)
+        @name = name
+        @salary = salary
+        @@all << self
+    end
+
+    def tax_bracket
+        self.class.all.select {|employee| employee.salary < (self.salary + 1000) && employee.salary > (self.salary - 1000)}
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(salary_amount)
+        self.all.select {|employee| employee.salary > salary_amount}
+    end
+
+    def self.find_by_department(deparment_name)
+        self.all.select {|employee| employee.department == deparment_name}
+    end
 end
--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,33 @@
 class Manager
+    attr_reader :name, :age, :department
+    @@all = []
 
+    def initialize(name, age, department)
+        @name = name
+        @age = age
+        @department = department
+        @@all << self
+    end
+
+    def employees
+        Employee.all.select {|employee| employee.manager == self}
+    end
+
+    def hire_employee(name, salary)
+        new_employee = Employee.new(name, salary)
+        new_employee.manager = self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.all_departments
+        self.all.collect {|manager| manager.department}
+    end
+
+    def self.average_age
+        manager_ages = self.all.collect {|manager| manager.age}
+        manager_ages.sum / manager_ages.length.to_f
+    end
 end
